ccde1931921ffd30b80163e1f792f6e0fed63752
zhouwei <zhouwei@xazenith.com>
2019-12-09 13:47:01 +0800

    did http service and sql operate

--- src/model/Sick.go
+++ src/model/Sick.go
@@ -3,7 +3,6 @@ package model
 import (
 	"database/sql"
 	"log"
-	"strings"
 	"time"
 )
 
@@ -36,56 +35,62 @@ type Sickness struct {
 	CreateAt time.Time `json:"create_at"`
 }
 
-var SicknessTableName = "sickness"
+var SicknessTableName = "public.data_sickness"
 
-func FetchSickById(id int64) (sick Sickness, err error) {
-	sick = Sickness{}
-	row := Db.QueryRow("SELECT "+SickItems("")+" from "+SicknessTableName+" where id = $1", id)
-	ScanRow(row, &sick)
-	return Sickness{}, nil
+func FetchSickById(id int64) (sick *Sickness, err error) {
+	sick = &Sickness{}
+	row := Db.QueryRow("SELECT id, uuid, name, symptom, nursing, medicals, duration, risks, create_at from public.data_sickness where id = $1", id)
+	ScanRow(row, sick)
+	return sick, nil
 }
 
 func FetchBySymptom(symptom string) (sicks []Sickness, err error) {
-
-	rows, err := Db.Query("SELECT "+SickItems("")+" FROM "+SicknessTableName+" where symptom like %$1%", symptom)
+	syp := "%" + symptom + "%"
+	rows, err := Db.Query("SELECT id, uuid, name, symptom, nursing, medicals, duration, risks, create_at FROM public.data_sickness where symptom like $1", syp)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	ScanRows(rows, sicks)
 	return
 }
 
 func (sick *Sickness) Save() (err error) {
-	stt := "insert into " + SicknessTableName + " (" + SickToSaveItems() + ") values ($1, $2, $3, $4, $5, $6) returning id, createAt"
+	if sick.Id == 0 {
+		err = sick.doCreate()
+	} else {
+		err = sick.doUpdate()
+	}
+	return
+}
+
+func (sick *Sickness) doCreate() (err error) {
+	stt := "insert into public.data_sickness (uuid, name, symptom, nursing, medicals, duration, risks, create_at) values ($1, $2, $3, $4, $5, $6, $7) returning id"
 	stmt, err := Db.Prepare(stt)
 	if stmt == nil {
-		log.Fatalln("statement is nil in save sick, " + sick.Name)
+		log.Println("statement is nil in save sick, " + sick.Name)
 		return
 	}
-	row := stmt.QueryRow(sick.Name, sick.Symptom, sick.Nursing, sick.Medicals, sick.Duration, sick.Risks)
+	row := stmt.QueryRow(createUUID(), sick.Name, sick.Symptom, sick.Nursing, sick.Medicals, sick.Duration, sick.Risks, time.Time{})
 	err = row.Scan(sick.Id, sick.CreateAt)
 	return
 }
 
-func SickToSaveItems() string {
-	return "`name`, symptom, nursing, medicals, duration, risks"
-}
-
-func SickItems(tag string) string {
-	if len(tag) > 0 {
-		return strings.Join([]string{
-			tag + ".id",
-			tag + ".uuid",
-			tag + ".`name`",
-			tag + ".symptom",
-			tag + ".nursing",
-			tag + ".medicals",
-			tag + ".duration",
-			tag + ".risks",
-			tag + ".createAt",
-		}, " ")
+func (sick *Sickness) doUpdate() (err error) {
+	var sk *Sickness
+	sk, err = FetchSickById(sick.Id)
+	if err != nil {
+		log.Printf("update sickness error, on reading sickness info by id: %d", sick.Id)
+		log.Println(err)
+		return
 	}
-	return "id, uuid,`name`, symptom, nursing, medicals, duration, risks, createAt"
+	stt := "update public.data_sickness set name=$2, symptom=$3, nursing=$4, medicals=$5, duration=$6, risks=$7 where id = $1"
+	stmt, err := Db.Prepare(stt)
+	if stmt == nil {
+		log.Println("statement is nil in save sick, " + sick.Name)
+		return
+	}
+	_ = stmt.QueryRow(sk.Id, sick.Name, sick.Symptom, sick.Nursing, sick.Medicals, sick.Duration, sick.Risks)
+	return
 }
 
 func ScanRows(rows *sql.Rows, sicks []Sickness) {
@@ -106,6 +111,6 @@ func ScanRows(rows *sql.Rows, sicks []Sickness) {
 func ScanRow(row *sql.Row, sick *Sickness) {
 	err := row.Scan(sick.Id, sick.Uuid, sick.Name, sick.Symptom, sick.Nursing, sick.Medicals, sick.Medicals, sick.Duration, sick.Risks)
 	if err != nil {
-		log.Fatalln("scan row error", err)
+		log.Println("scan row error", err)
 	}
 }
--- src/server/Server.go
+++ src/server/Server.go
@@ -2,38 +2,119 @@ package server
 
 import (
 	"../model"
-	"fmt"
-	"net/http"
-	"log"
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
-var app_address string = "0.0.0.0:8001"
+var appAddress string = "0.0.0.0:8001"
 
 func init() {
 	mux := http.NewServeMux()
 	server := http.Server{
-		Addr: app_address,
+		Addr:    appAddress,
 		Handler: mux,
 	}
 	mux.HandleFunc("/", handler)
 	mux.HandleFunc("/sickness", sicknessHandler)
 	_ = server.ListenAndServe()
-	log.Printf("server is running at: %s", app_address)
+	log.Printf("server is running at: %s", appAddress)
 }
 
 func handler(repWriter http.ResponseWriter, req *http.Request) {
 	log.Printf("resolve request from %s", req.URL)
 }
 
-func sicknessHandler(repWriter http.ResponseWriter, req *http.Request) {
+func sicknessHandler(repWriter http.ResponseWriter, req *http.Request){
 	switch req.Method {
 	case "GET":
-		sick := model.Sickness{}
-		byt,_ := json.Marshal(&sick)
-		repWriter.Write(byt)
-		return;
+		sicknessFetchHandler(repWriter, req)
+		return
+	case "POST":
+		sicknessSaveHandler(repWriter, req)
+		return
+	case "DELETE":
+
+	case "PUT":
+
 	}
+}
+
+func sicknessSaveHandler(repWriter http.ResponseWriter, req *http.Request) {
+	l := req.ContentLength
+	body := make([]byte, l)
+	_, err := req.Body.Read(body)
+	if err != nil {
+		log.Println("reading request body error .", err)
+	}
+	var sick model.Sickness
+	err = json.Unmarshal(body, &sick)
+	if err != nil {
+		log.Println("parsing request json body error .", err)
+	}
+	err = sick.Save()
+	if err != nil {
+		log.Println("saving sick info occur error. ", err)
+	}else {
+		log.Println("saved sickness info,", sick)
+	}
+	_ = writeSicknessToResponse(repWriter, &sick)
+}
+
+func sicknessFetchHandler(repWriter http.ResponseWriter, req *http.Request) {
+
+	sick, err := parseAndFetchSickness(req)
+	if err != nil {
+		log.Println("find sick info error. ", err)
+		return
+	}
+	err = writeSicknessToResponse(repWriter, sick)
+	if err != nil {
+		log.Println("error occurred on write response")
+	}
+}
+
+func parseAndFetchSickness(req *http.Request) (sick *model.Sickness, err error) {
+	err = req.ParseForm()
+	if err != nil {
+		return
+	}
+	form := req.Form
+	id := form.Get("id")
+	var sickId int64
+	sickId, err = strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		return
+	}
+	sick, err = model.FetchSickById(sickId)
+	return
+}
+
+func writeSicknessToResponse(repWriter http.ResponseWriter, sick *model.Sickness) (err error) {
+	var btys []byte
+	btys, err = writeSicknessToJson(sick)
+	if err != nil {
+		log.Println("write sickness to json error.")
+		return err
+	}
+	_, err = repWriter.Write(btys)
+	if err != nil {
+		log.Println("write sickness json to http response error.")
+		return err
+	}
+	return nil
+}
+
+func writeSicknessToJson(sick *model.Sickness) (res []byte, err error) {
+	res, err = json.Marshal(sick)
+	return
+}
+
+func writeSicknessListToJson(sicks *[]model.Sickness) (res []byte, err error) {
+	res, err = json.Marshal(sicks)
+	return
 }
 
 func authenticate(w http.ResponseWriter, r *http.Request) {
